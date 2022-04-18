effb597bd3bd03146c26b33ccf428f09837135c2
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2022-04-18 11:06:57 +0900

    Add np(NowPlaying) function

--- src/Example.Config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    token: "Discord_Bot_Token",
-    prefix: "!",
-    owner: "Your_Discord_User_Id",
-    debug: true,
-    refresh: false
-};
--- src/Index.ts
+++
@@ -1,9 +0,0 @@
-import Client, { intents } from './Class';
-import config from './Config';
-
-const client = new Client({
-    intents: intents,
-    partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"]
-});
-
-client.login(config.token);
